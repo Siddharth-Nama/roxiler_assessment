f79fb4148b61d67a8ee7b146afbfbff88780534b
Siddharth-Nama <siddharthnama2604@gmail.com>
2026-03-22 21:32:59 +0530

    Subtask 4: User Model - Custom User model with Roles defined

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    SYSTEM_ADMIN = 'Admin'
+    NORMAL_USER = 'User'
+    STORE_OWNER = 'StoreOwner'
+    
+    ROLE_CHOICES = [
+        (SYSTEM_ADMIN, 'System Administrator'),
+        (NORMAL_USER, 'Normal User'),
+        (STORE_OWNER, 'Store Owner'),
+    ]
+    
+    name = models.CharField(max_length=60)
+    address = models.TextField(max_length=400)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default=NORMAL_USER)
+    
+    # Email will be used for login, so we make it unique
+    email = models.EmailField(unique=True)
+    
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'name']
+
+    def __str__(self):
+        return self.email
